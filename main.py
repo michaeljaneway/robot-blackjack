e3e7e196100944fae4df2033bbce6ef4d37a87e9
Happyfacemike <michael.janeway03@gmail.com>
2020-09-08 09:37:54 -0400

    Added Button.py to main.py

--- main.py
+++ main.py
@@ -32,6 +32,55 @@ import keyboard
 # (Can only play one .wav sound file at a time, but can stop using None as its filename
 import winsound
 
+class Button:
+    """A button is a labeled rectangle in a window. It is activated or
+    deactivated with the activate() and deactivate() methods. The clicked(p)
+    method returns true if the button is active and p is inside it."""
+
+    def __init__(self, win, center, width, height, label):
+        """ Creates a rectangular button, eg:qb = Button(myWin, centerPoint,
+        width, height, 'Quit') """
+        w,h = width/2.0, height/2.0
+        x,y = center.getX(), center.getY()
+        self.xmax, self.xmin = x+w, x-w
+        self.ymax, self.ymin = y+h, y-h
+        p1 = Point(self.xmin, self.ymin)
+        p2 = Point(self.xmax, self.ymax)
+        self.rect = Rectangle(p1,p2)
+        self.rect.setFill('lightgray')
+        self.rect.draw(win)
+        self.label = Text(center, label)
+        self.label.draw(win)
+        self.deactivate()
+
+    def clicked(self, p):
+        """Returns true if button active and p is inside"""
+        if not p == None:
+            return (self.active and
+            self.xmin <= p.getX() <= self.xmax and
+            self.ymin <= p.getY() <= self.ymax)
+
+    def getLabel(self):
+        """Returns the label string of this button."""
+        return self.label.getText()
+
+    def activate(self):
+        """Sets this button to 'active'."""
+        self.label.setFill('black')
+        self.rect.setWidth(1)
+        self.active = True
+
+    def deactivate(self):
+        """Sets this button to 'inactive'."""
+        self.label.setFill('darkgrey')
+        self.rect.setWidth(0)
+        self.active = False
+
+    def delete(self):
+        """Undraws the button"""
+        self.deactivate()
+        self.label.undraw()
+        self.rect.undraw()
 
 class Encounter:
     def __init__(self, encounter_num):
